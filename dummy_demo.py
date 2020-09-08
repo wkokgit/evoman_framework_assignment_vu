d60a88ef72c0a7c99d74f5281d9ee75fa955d1af
wkokgit <wouterkok96@hotmail.com>
2020-09-08 09:36:47 +0200

    commented structure of dummy_demo algo and added gitignore

--- dummy_demo.py
+++ dummy_demo.py
@@ -6,14 +6,39 @@
 
 # imports framework
 import sys, os
-sys.path.insert(0, 'evoman') 
+sys.path.insert(0, 'evoman')
 from environment import Environment
+from demo_controller import player_controller
+
 
 experiment_name = 'dummy_demo'
 if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
 
-# initializes environment with ai player using random controller, playing against static enemy
-env = Environment(experiment_name=experiment_name)
+n_hidden_neurons = 3
+
+# initializes simulation in individual evolution mode, for single static enemy.
+env = Environment(experiment_name=experiment_name,
+                  enemies=[2],
+                  playermode="ai",
+                  #player_controller=player_controller(n_hidden_neurons),
+                  enemymode="static",
+                  level=2,
+                  speed="fastest")
 env.play()
+
+# DEAP
+# https://deap.readthedocs.io/en/master/
+# https://deap.readthedocs.io/en/master/examples/ga_onemax.html
+
+
+# INITIALIZATION
+
+# GENERATION
+#population
+# VARIATION
+
+# EVALUATION
+
+# SELECTION
 
