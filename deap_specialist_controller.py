f44001dc1d31cdc7244f3638fc09406f099e2e6a
wkokgit <wouterkok96@hotmail.com>
2020-09-09 14:56:08 +0200

    added deap_specialist_controller that reads the best solution and is runnable

--- deap_specialist_controller.py
+++ deap_specialist_controller.py
@@ -14,7 +14,7 @@ from deap import base
 from deap import creator
 from deap import tools
 import array
-import numpy
+import numpy as np
 
 from deap import algorithms
 from environment import Environment
@@ -25,19 +25,20 @@ experiment_name = "dummy_demo"
 if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
 
-n_hidden_neurons = 3
+n_hidden_neurons = 10
 
 # initializes simulation in individual evolution mode, for single static enemy.
 env = Environment(
     experiment_name=experiment_name,
     enemies=[2],
     playermode="ai",
-    # player_controller=player_controller(n_hidden_neurons),
+    player_controller=player_controller(n_hidden_neurons),
     enemymode="static",
     level=2,
-    speed="fastest",
+    speed="normal",
 )
 
-
+sol = np.loadtxt('deap_specialist_optimization/best.txt')
+print('\n LOADING SAVED SPECIALIST SOLUTION FOR ENEMY 2 \n')
 # READ SOLUTION
-env.play()
+env.play(sol)
